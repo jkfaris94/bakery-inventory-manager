98f3df663e05bfffee5634446d5430af7712ac49
Johnny Faris <jkfaris@gmail.com>
2025-06-30 16:44:36 -0700

    brushed up back end recipe router

--- backend/src/recipes/recipes.router.js
+++ backend/src/recipes/recipes.router.js
@@ -1,15 +1,20 @@
 const express = require("express");
+const router = express.Router();
 const controller = require("./recipes.controller");
 const recipeIngredientsRouter = require("./recipe_ingredients.router");
-const router = express.Router();
+const methodNotAllowed = require("../errors/methodNotAllowed");
 
 router
-  .get("/", controller.list)
-  .post("/", controller.create);
+  .route("/")
+  .get(controller.list)
+  .post(controller.create)
+  .all(methodNotAllowed);
 
 router
-  .get("/:id", controller.read)
-  .delete("/:id", controller.delete);
+  .route("/:id")
+  .get(controller.read)
+  .delete(controller.delete)
+  .all(methodNotAllowed);
 
 // POST /recipes/:id/bake
 router.post("/:id/bake", controller.bake);
