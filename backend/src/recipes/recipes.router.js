e3d5f646db101cad4bfffe1d5cda9777c56df9ca
Johnny Faris <johnnyfaris@Johnnys-MacBook-Pro.local>
2025-07-07 01:15:48 -0700

    removed method not allowed handler for now

--- backend/src/recipes/recipes.router.js
+++ backend/src/recipes/recipes.router.js
@@ -2,19 +2,16 @@ const express = require("express");
 const router = express.Router();
 const controller = require("./recipes.controller");
 const recipeIngredientsRouter = require("./recipe_ingredients.router");
-const methodNotAllowed = require("../errors/methodNotAllowed");
 
 router
   .route("/")
   .get(controller.list)
   .post(controller.create)
-  .all(methodNotAllowed);
 
 router
   .route("/:id")
   .get(controller.read)
   .delete(controller.delete)
-  .all(methodNotAllowed);
 
 // POST /recipes/:id/bake
 router.post("/:id/bake", controller.bake);
